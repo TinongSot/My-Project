2fbdd63781cc666a2792ab991065a654e535d48a
Tinong Sot <tinong.sot@student.niptict.edu.kh>
2022-07-13 15:00:00 +0700

    update

--- src/create_page.php
+++ src/create_page.php
@@ -1,9 +1,21 @@
+<?php
+include 'db_connect.php';
+$query = $fluent->from('products'); 
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $amount = $_POST['amount'];
+  $price = $_POST['price'];
+
+  $sql = "INSERT INTO products (name, amount, price) VALUES ('$name', '$amount', '$price')";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
 <body>
