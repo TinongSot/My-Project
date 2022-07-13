b5a8314b1727ee4bab78dac6c0a1db1b4a4237f5
Tinong Sot <tinong.sot@student.niptict.edu.kh>
2022-07-13 16:33:04 +0700

    fix login and register

--- register_project/table.php
+++ register_project/table.php
@@ -7,7 +7,7 @@
   <title>User Table</title>
 </head>
 <body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <script type="text/javascript">
             $(function(){
                 alert('You have successfully register!! Click "OK" for an other registration');
